039a027b757faae81f64edad5656b9b68b58d839
Pablo Jesus da Silva <pablojesusdasilva0@gmail.com>
2023-09-04 17:07:44 -0300

    :feat: Criando seção sobre a La Pizza

--- src/app/components/Card.jsx
+++ src/app/components/Card.jsx
@@ -7,10 +7,10 @@ export default function Card({ image, title }) {
         <Image src={image} />
       </div>
 
-      <h1 className='my-4 font-semibold text-xl'>{title}</h1>
+      <h1 className='my-6 font-semibold text-xl'>{title}</h1>
       <p className='w-52 tracking-wide text-zinc-700' >Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
 
-      <span className='my-10 text-4xl font-bold text-zinc-800'>R$ 23</span>
+      <span className='my-12 text-4xl font-bold text-zinc-800'>R$ 23</span>
 
       <button className='py-2 px-5 text-lg rounded-full font-semibold bg-yellow-500'>Comprar agora</button>
     </div>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,16 +8,17 @@ import PizzaCuma from '../img/pizza1.png';
 import PizzaMenta from '../img/pizza2.png';
 import PizzaBana from '../img/pizza3.png';
 import PizzaToca from '../img/pizza4.png';
+import Store from '../img/store.png';
 
 export default function Home() {
   return (
     <>
       <Header image={Banner} />
 
-      <main className='w-full flex flex-col items-center gap-4 my-16'>
+      <main className='w-full flex flex-col items-center gap-4 my-20'>
         <h1 className='text-4xl font-semibold'>Mais Vendidas</h1>
 
-        <section className='flex justify-center gap-x-10 mt-4'>
+        <section className='flex justify-center gap-x-10 mt-16'>
           <Card 
             image={PizzaCuma}
             title='La Cuma' 
@@ -34,6 +35,26 @@ export default function Home() {
             image={PizzaToca}
             title='La Toca' 
           />
+        </section>
+
+        <section className='grid grid-cols-2 w-3/4 mt-40'>
+          <Image src={Store} alt='Foto da fachada da loja' />
+
+          <div className='flex flex-col justify-center ml-8 gap-y-12'>
+            <h1 className='text-4xl font-semibold tracking-wide'>
+              Sobre a La P 
+              <span className='text-yellow-500'>i</span>
+              <span className='text-red-500'>z</span>
+              <span className='text-green-500'>z</span>
+              a
+            </h1>
+            <p className='tracking-wider leading-7'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis justo sem. Donec consequat, nunc a dapibus dapibus, justo dolor tristique tortor, sit amet tincidunt lorem nisl at ex. Cras dolor purus, varius in euismod pretium, finibus nec mi. Curabitur aliquam pharetra lectus in pharetra. Nullam viverra elementum neque quis fermentum. 
+            </p>
+            <button className='w-40 py-2 text-lg rounded-full font-semibold bg-yellow-500'>
+              Ler mais
+            </button>
+          </div>
         </section>
       </main>
     </>
